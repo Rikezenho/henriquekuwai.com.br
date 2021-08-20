8a644a3f3ba3602436be3e01fc63eecccc3a15b9
Henrique Kuwai <rikezenho@gmail.com>
2021-08-20 01:11:26 -0300

    Add google fonts and global styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,38 @@
 import React from "react";
 import Head from "next/head";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { globals } from "../globals";
 import "../styles/base.css";
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+    text: {
+      primary: "#444",
+    },
+  },
+};
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Fira Sans', Helvetica, sans-serif;
+    line-height: 1.4;
+    font-size: 1rem;
+    color: ${theme.colors.text.primary};
+  }
+`;
 
 const App: React.FC = ({ Component, pageProps }: any) => {
   return (
     <div className="container">
       <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Fira+Sans:300,400,700"
+        />
         {globals.googleAnalyticsId && (
           <script
             async
@@ -27,7 +53,10 @@ const App: React.FC = ({ Component, pageProps }: any) => {
           ></script>
         )}
       </Head>
-      <Component {...pageProps} />
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </div>
   );
 };
