2d418fa01a6fb41f933ab622504ea08687eef1ba
Henrique Kuwai <rikezenho@gmail.com>
2021-08-21 03:21:02 -0300

    add CvJob

---
+++ src/components/CvJob/CvJob.tsx
@@ -0,0 +1,28 @@
+import { Employee } from "../../data";
+import { AiOutlineTrophy } from "react-icons/ai";
+
+const CvJob: React.FC<Employee> = (props: Employee) => {
+  const { date, position, provider, description, awards } = props;
+
+  return (
+    <div className="job-item">
+      <div className="title">
+        <div className="date">{date}</div>
+        <span className="text">{position}</span>
+      </div>
+      <div className="provider">{provider}</div>
+      {description ? <div className="description">{description}</div> : null}
+      {awards?.length ? (
+        <div className="awards">
+          {awards.map((title, key) => (
+            <div key={key} className="award-item">
+              <AiOutlineTrophy /> <span className="info">{title}</span>
+            </div>
+          ))}
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export { CvJob };
