30240b02882902476837bb129e3fc2b118b68196
Henrique Kuwai <rikezenho@gmail.com>
2021-08-27 01:37:19 -0300

    add locales in next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 module.exports = {
+  i18n: {
+    locales: ["en", "pt-BR"],
+    defaultLocale: "pt-BR",
+  },
   trailingSlash: true,
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
-      use: 'raw-loader',
+      use: "raw-loader",
     });
     config.resolve.alias = {
       ...config.resolve.alias,
-      '~': __dirname,
+      "~": __dirname,
     };
     return config;
-  }
+  },
 };
